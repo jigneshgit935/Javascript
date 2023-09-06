abe9905e9673ad892a51dedbedb9d8486ebbb9a3
Jignesh <jignesh.sharma.707@gmail.com>
2023-09-06 06:56:11 +0530

    Operation

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -27,3 +27,30 @@ let sumNumber = 23;
 let convertNumbertoString = String(sumNumber); //=>33
 // console.log( convertNumbertoString); => 33
 console.log(typeof convertNumbertoString); // =>string
+
+// ==================Operation===============================//
+let value = 1;
+let negValue = -value;
+// console.log(negValue); //=> -1
+
+// console.log(2 % 6);
+
+let str1 = 'jignesh';
+let str2 = ' sharma';
+let str3 = str1 + str2;
+console.log(str3);
+
+// console.log('1' + '2');
+// console.log('1' + 2);
+// console.log(1 + '2');
+// console.log(1 + 2);
+// console.log('1' + 2 + 2);
+// console.log(1 + 2 + '2');
+
+// console.log(+true); // 1
+// console.log(true+); //error
+// console.log(+''); // 0
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter); // 101
