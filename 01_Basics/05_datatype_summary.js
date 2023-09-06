adf09f05e16af4cfabe66974ed23d75fca49cf5b
Jignesh <jignesh.sharma.707@gmail.com>
2023-09-06 17:14:45 +0530

    datatypes-summary

---
+++ 01_Basics/05_datatype_summary.js
@@ -0,0 +1,26 @@
+// primitive
+// ==> 7 types primitive types
+
+/*
+String , Number , Boolean , Undefined , Null , Symbol , Bigint
+*/
+
+// Non Primitive or Reference
+/*
+memory mai inka reference direct allocate kiya jaa skta hai
+
+
+// Array , Object , Function
+
+
+const heros =["shaktiman",doga","hanuman"]
+
+let myObj={
+    name:"jinu",
+    age:"21"
+}
+
+const funtion = funtion(){
+    console.log("Hello World")
+}
+*/
