67425f055102c0e0bab27ac39593a18f2c491126
Jignesh <jignesh.sharma.707@gmail.com>
2023-09-06 06:28:01 +0530

    datatypes

---
+++ 01_Basics/01_datatypes.js
@@ -0,0 +1,28 @@
+'use strict'; //treat all js a newer version
+
+// alert(3+3) we are using nodejs, not browser
+
+// code readability should be high
+console.log(3 + 3);
+console.log('Jignesh');
+
+let name = 'jignesh'; //string
+let age = 23; //number
+let logged = true; //boolean
+let state = null; //standlone value
+
+// datatype
+// number
+// bigint
+// string
+// boolean true/false
+// null =>standlone value ::::representation of empty value
+// undefined==>when value is not assigned
+// symbol => unique
+// end---primitive datatypes
+
+// object
+console.log(typeof 2);
+console.log(typeof 'jignesh');
+console.log(typeof null); //---its an object (it is a type)
+console.log(typeof undefined);
---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,18 @@
+const name = 'jignesh';
+// name = 'sharma';
+
+// Cannot change const after once declared
+
+let id = '12344';
+id = '545467';
+// we can change let keyword
+
+var email = 'jignes@google.com';
+
+// now var keyword is deprecated , we do not have to use it anymore
+accoutNo = '22222';
+accoutNo = '545454545454545';
+
+// we should use this type, it is never recommoned
+
+console.table([id, email, accoutNo]);
