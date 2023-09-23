62b3162d05785bf2aead761d9b8d0e8d010475a8
Jignesh <jignesh.sharma.707@gmail.com>
2023-09-23 17:20:45 +0530

    control flow in js

---
+++ 05_basics/if.js
@@ -0,0 +1,55 @@
+// if
+
+// if (true) {
+// }
+// if (false) {
+// }
+
+// const temp = 244;
+// if (temp < 50) {
+//   console.log('temperature is less than 50 ');
+// } else {
+//   console.log('Greater then 50');
+// }
+
+//
+
+// const score = 300;
+
+// if (score > 100) {
+//   const power = 'fly';
+//   console.log(`Power is: ${power}`);
+// }
+
+//
+// const bal = 4000;
+
+// if (bal > 1000) console.log('test');
+
+//
+// const balance = 1000;
+// if (balance < 500) {
+//   console.log('Less than 500');
+// } else if (balance < 750) {
+//   console.log('Less than 750');
+// } else {
+//   console.log('less than 1200');
+// }
+
+//
+// const userLoggedIn = true;
+// const debitCard = false;
+
+// if (userLoggedIn && debitCard) {
+//   console.log('you are allow to buy course');
+// } else {
+//   console.log('you are not allow to buy course');
+// }
+
+//
+
+const userLoggedInFromGoogle = false;
+const userLoggedInFromEmail = true;
+if (userLoggedInFromEmail || userLoggedInFromEmail) {
+  console.log('User logged in');
+}
---
+++ 05_basics/switch.js
@@ -0,0 +1,73 @@
+// const month = 4;
+
+// switch (month) {
+//   case 1:
+//     console.log('january');
+//     break;
+//   case 2:
+//     console.log('February');
+//     break;
+//   case 3:
+//     console.log('march');
+//     break;
+//   case 4:
+//     console.log('april');
+//   // break;
+
+//   default:
+//     console.log('no months');
+//     break;
+// }
+
+//
+
+// truthy values
+// const email = 'jinu@gmail.com';
+// const email = '';
+// if (email) {
+//   console.log('Got a user');
+// } else {
+//   console.log("Don't have an user");
+// }
+
+// truthy values = "0" , "false" , " (space)" , [] , {} , function() ,
+
+// falsy values = false , 0 , -0 , BigInt 0n , "" , null , undefined , NaN
+
+//
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");
+// }
+
+// let emptyObject = { name: 'jinu' };
+// if (Object.keys(emptyObject).length === 0) {
+//   console.log('Object is empty');
+// } else {
+//   console.log('Object is not empty');
+// }
+
+//
+// /
+
+// let check = false == 0;
+// console.log(check);
+
+// let check1 = false == '';
+// console.log(check1);
+
+////////////////////////////
+
+// Nullish Coalishing Operator (??): null undefined
+// let val1;
+// // val1 = 3 ?? 4;
+// // val1 = null ?? 4;
+// val1 = null ?? 2 ?? 4;
+// console.log(val1);
+
+// ternary operator
+
+// conditon ? true : false;
+
+// let number = 2;
+// const resutl = number === 1 ? true : false;
+// console.log(resutl);
