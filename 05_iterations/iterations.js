9a324f24cdf910451b11a6be37e4e4e03b1398a7
Jignesh <jignesh.sharma.707@gmail.com>
2023-09-25 17:50:25 +0530

    iteration break and continure

---
+++ 05_iterations/iterations.js
@@ -0,0 +1,56 @@
+//for
+
+// for (let i = 0; i <= 10; i++) {
+//   const element = i;
+//   if (element == 5) {
+//     console.log('5 is best number');
+//   }
+//   console.log(element);
+// }
+
+// ////
+// for (let i = 0; i <= 10; i++) {
+//   console.log(`Outer loop: ${i}`);
+//   for (let j = 0; j <= 10; j++) {
+//     console.log(`inner loop: ${j} and inner loop ${i}`);
+//   }
+// }
+
+////////////////////
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`Outer loop ${i}`);
+
+//   for (let j = 1; j <= 10; j++) {
+//     console.log(`${i} + ${j} = ${i * j}`);
+//   }
+// }
+
+// ////////////////////
+
+// const myArray = ['Jinu', 'Rahul', 'Jignesh', 'Manvik'];
+// console.log(myArray.length);
+// for (let index = 0; index <= myArray.length - 1; index++) {
+//   const element = myArray[index];
+//   console.log(element);
+// }
+
+////////keywords=> break and continue
+// break
+// for (let i = 1; i <= 20; i++) {
+//   if (i == 5) {
+//     console.log(`Detected 5`);
+//     break;
+//   }
+
+//   console.log(`Value of i is ${i}`);
+// }
+
+// continue
+// for (let i = 1; i <= 20; i++) {
+//   if (i == 5) {
+//     console.log(`Detected 5`);
+//     continue;
+//   }
+
+//   console.log(`Value of i is ${i}`);
+// }
