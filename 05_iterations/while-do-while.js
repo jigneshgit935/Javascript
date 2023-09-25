57d9b452d87b62aef66e68aa17470caeb04f0786
Jignesh <jignesh.sharma.707@gmail.com>
2023-09-25 18:00:21 +0530

    do while loop

---
+++ 05_iterations/while-do-while.js
@@ -0,0 +1,22 @@
+// while loop
+
+// let i = 0;
+// while (i <= 10) {
+//   console.log(`Value of i is ${i}`);
+//   i = i + 2;
+// }
+
+// array
+// let array = ['A', 'B', 'C'];
+// let arr = 0;
+// while (arr < array.length) {
+//   console.log(array[arr]);
+//   arr++;
+// }
+
+//do while
+let score = 11;
+do {
+  console.log(`Score is ${score}`);
+  score = score + 1;
+} while (score <= 10);
